dd0f411dd737dfa8eebaa0165020851eb4cb5af2
Katrina Peterson <peterkat009@edmonds15.org>
2019-08-14 14:20:28 -0700

    some changes

--- selfCareHome.js
+++ selfCareHome.js
@@ -1,1 +1,8 @@
-
+function myFunction() {
+    document.getElementById("demo").style.color = "red";
+}
+<form>
+<input type=button
+value="demo"
+onClick="self.location='Your_URL_here.htm'">
+</form>
