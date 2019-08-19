54e089deee1444966bb8f01147aac5930a871d10
gwubbie <wubbie72@gmail.com>
2019-08-19 09:26:30 -0700

    adding files

--- finalproject.js
+++ finalproject.js
@@ -1,8 +1,17 @@
-function myFunction() {
-    document.getElementById("demo").style.color = "red";
+// function myFunction() {
+//     document.getElementById("demo").style.color = "red";
+// }
+// <form>
+// <input type=button
+// value="demo"
+// onClick="self.location='Your_URL_here.htm'">
+// </form>
+
+function displaytext() {
+  var x = document.getElementById("mentalhealth");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
-<form>
-<input type=button
-value="demo"
-onClick="self.location='Your_URL_here.htm'">
-</form>
