9469314fefc3bf31cd03108d623cfe0f696a5ab6
gwubbie <wubbie72@gmail.com>
2019-08-19 14:11:38 -0700

    blue

--- finalproject.js
+++ finalproject.js
@@ -15,3 +15,48 @@ function displaytext() {
     x.style.display = "none";
   }
 }
+
+function important() {
+  var x = document.getElementById("important");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function day() {
+  var x = document.getElementById("day");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function help() {
+  var x = document.getElementById("help");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function need() {
+  var x = document.getElementById("need");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+}
+function pink() {
+  var x = document.getElementById("pink");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
